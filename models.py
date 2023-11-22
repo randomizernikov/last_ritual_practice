243e36d7712308bcf03c6bef25311eb8f8d3f1b7
randomizernikov <ilyhidze3@gmail.com>
2023-11-22 17:43:48 +0300

    поработал с моделями

--- models.py
+++ models.py
@@ -1,26 +1,16 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base, engine
 
-class Vid_tovar(Base):
-    __tablename__ = "vid_tovar"
+class Product_type(Base):
+    __tablename__ = "product_type"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    product_type = Column(String)
 
-class Tovar(Base):
-    __tablename__ = "tovar"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name_tovar = Column(String)
-    vid_tovar_id = (Integer, ForeignKey("vid_tovar.id"))
-
-
-class Uslugi(Base):
-    __tablename__ = "uslugi"
+class Products(Base):
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
-    usluga = Column(String)
-    description = Column(String)
-
-
+    product = Column(String)
+    product_type_id = (Integer, ForeignKey("product_type.id"))
 
